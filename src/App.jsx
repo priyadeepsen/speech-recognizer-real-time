2b6383f8cb8c2bddf337b55ee14ebfb3e2233895
priyadeepsen <senbiswadip2345@gmail.com>
2024-07-09 23:09:28 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import useClipboard from "react-use-clipboard";
+import 'regenerator-runtime/runtime'
+import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const [textToCopy, setTextToCopy] = useState();
+    const [isCopied, setCopied] = useClipboard(textToCopy, {
+        successDuration:1000
+    });
+
+    //subscribe to thapa technical for more awesome videos
+
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+    if (!browserSupportsSpeechRecognition) {
+        return null
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br/>
+                <p>A React hook that converts speech from the microphone to text and makes it available to your React
+                    components.</p>
+
+                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                    {transcript}
+                </div>
+
+                <div className="btn-style">
+
+                    <button onClick={setCopied}>
+                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
+                    </button>
+                    <button onClick={startListening}>Start Listening</button>
+                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default App
